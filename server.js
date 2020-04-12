39a071a32e89aaa423247c470f5ddb8ac88289fc
turingcreep <shankar.sengupta.dev@gmail.com>
2020-04-11 21:45:13 -0700

    Adding meta-data tooltip.
    1. Moving all javascript within update.html to it's own javascript file
    called update.js.
    
    2. Allowing express to transparently server files within the bt_app
    directory.

--- server.js
+++ server.js
@@ -5,6 +5,10 @@ const port = 3000;
 
 app.get('/', (req, res) => res.sendFile(path.resolve(__dirname, 'bt_app/update.html')));
 
-app.get('/bt_app/hk_stocks.csv', (req, res) => res.sendFile(path.resolve(__dirname, 'bt_app/update.html')));
+//app.get('/bt_app/hk_stocks.csv', (req, res) => res.sendFile(path.resolve(__dirname, 'bt_app/update.html')));
 app.get('/hk_stocks.csv', (req, res) => res.sendFile(path.resolve(__dirname, 'bt_app/hk_stocks.csv')));
+app.get('/:fileName', (req, res) => {
+  const { params: { fileName } } = req;
+  return res.sendFile(path.resolve(__dirname, `bt_app/${fileName}`));
+});
 app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`));
